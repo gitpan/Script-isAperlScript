f5e2ec78ffe1cc9d310228bade8757f08381bf57
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2012-08-04 13:08:07 +0000

    Import of VVELOX/Script-isAperlScript-1.0.0 from CPAN.
    gitpan-cpan-distribution: Script-isAperlScript
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         VVELOX/Script-isAperlScript-1.0.0.tar.gz
    gitpan-cpan-author:       VVELOX
    gitpan-cpan-maturity:     released

--- lib/Script/isAperlScript.pm
+++ lib/Script/isAperlScript.pm
@@ -2,14 +2,7 @@ package Script::isAperlScript;
 
 use warnings;
 use strict;
-use Exporter;
-
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-
-our @ISA         = qw(Exporter);
-our @EXPORT      = qw(isAperlScript stringIsAperlScript);
-our @EXPORT_OK   = qw(isAperlScript stringIsAperlScript);
-our %EXPORT_TAGS = (DEFAULT => [qw(isAperlScript stringIsAperlScript)]);
+use base 'Error::Helper';
 
 =head1 NAME
 
@@ -17,18 +10,17 @@ Script::isAperlScript - This does a basic check if something is a perl script or
 
 =head1 VERSION
 
-Version 0.0.0
+Version 1.0.0
 
 =cut
 
-our $VERSION = '0.0.0';
-
+our $VERSION = '1.0.0';
 
 =head1 SYNOPSIS
 
 This module does a basic check to see if something is a perl script.
 
-It will currently check for the matches below.
+By default it checks for the paths below.
 
     /^\#\!\/usr\/bin\/perl/
     /^\#\!\/usr\/bin\/suidperl/
@@ -37,15 +29,72 @@ It will currently check for the matches below.
 
 This will also match stuff like "#!/usr/local/bin/perl5.8.9".
 
-More will be added eventually.
+If {env=>1} is given to the new method, the checks below are done.
 
-=head1 EXPORT
+    /^\#!\/usr\/bin\/env.*perl/
 
-isAperlScript
+If {any=>1} is given to the new method, the checks below are done.
 
-stringIsAperlScript
+    /^\#!\/.*perl/
 
-=head1 FUNCTIONS
+=head1 METHODS
+
+=head2 new
+
+This initiates the object.
+
+=head3 args hash
+
+=head4 env
+
+Allow env based scripts.
+
+By default this is false as it can't be trusted.
+
+=head4 any
+
+This does very loose matching.
+
+By default this is false as it can't be trusted.
+
+    my $checker=Script::isAperlScript->new( \%args );
+
+=cut
+
+sub new{
+	my %args;
+	if ( defined( $_[1] ) ){
+		%args=%{$_[1]};
+	}
+
+	my $self={
+		error=>0,
+		errorString=>0,
+		env=>0,
+		any=>0,
+		errorExtra=>{
+			flags=>{
+				2=>'noString',
+				3=>'doesNotExist',
+				4=>'notReadable',
+				5=>'fileNotSpecified',
+				6=>'noFile',
+				7=>'notAfile',
+			},
+		},
+	};
+	bless $self;
+
+	if ( $args{env} ){
+		$self->{env}=1;
+	}
+
+	if ( $args{any} ){
+		$self->{any}=1;
+	}
+
+	return $self;
+}
 
 =head2 isAperlScript
 
@@ -63,36 +112,51 @@ In regards to the returned value, see the section "RETURN" for more information.
 =cut
 
 sub isAperlScript{
-	my $file=$_[0];
+	my $self=$_[0];
+	my $file=$_[1];
+
+	if ( ! $self->errorblank ){
+		return undef;
+	}
 
 	#make sure a file is specified
 	if (!defined($file)) {
-		return 5;
+		$self->{error}=5;
+		$self->{errorString}='No file defined';
+		return undef;
 	}
 	#make sure it exists
 	if (! -e $file) {
-		return 3;
+		$self->{error}=3;
+		$self->{errorString}='The file, "'.$file.'", does not exist';
+		return undef;
 	}
 
 	#it is not a file
 	if (! -f $file) {
-		return 7;
+		$self->{error}=7;
+		$self->{errorString}='"'.$file.'" is not a file';
+		return undef;
 	}
 
 	#make sure it is readable
 	if (! -r $file) {
-		return 4;
+		$self->{error}=4;
+		$self->{errorString}='"'.$file.'" is not readable';
+		return undef;
 	}
 
 	#try to open it
 	if (open(THEFILE, '<', $file )) {
 		my $string=join("", <THEFILE>);
 		close(THEFILE);
-		return stringIsAperlScript($string);
+		return $self->stringIsAperlScript($string);
 	}
 
 	#it could not be opened
-	return 6;
+	$self->{error}=6;
+	$self->{errorString}='"'.$file.'" could not be opened';
+	return undef;
 }
 
 =head2 stringIsAperlScript
@@ -111,76 +175,91 @@ In regards to the returned value, see the section "RETURN" for more information.
 =cut
 
 sub stringIsAperlScript{
-	my $string=$_[0];
+	my $self=$_[0];
+	my $string=$_[1];
+
+	if ( ! $self->errorblank ){
+		return undef;
+	}
 
 	#make sure a string is specified
 	if (!defined( $string )) {
-		return 2;
+		$self->{error}=2;
+		$self->{errorString}='No string defined';
+		return undef;
+	}
+
+	#check if it should possibly do the any check
+	if ( $self->{any} ){
+		if ($string =~ /^\#\!\/.*perl/) {
+			return 1;
+		}
 	}
 	
 	#checks if it makes #!/usr/bin/perl
 	if ($string =~ /^\#\!\/usr\/bin\/perl/) {
-		return 0;
+		return 1;
 	}
 
 	#checks if it makes #!/usr/bin/suidperl
 	if ($string =~ /^\#\!\/usr\/bin\/suidperl/) {
-		return 0;
+		return 1;
 	}
 
 	#checks if it makes #!/usr/local/bin/perl
 	if ($string =~ /^\#\!\/usr\/local\/bin\/perl/) {
-		return 0;
+		return 1;
 	}
 
 	#checks if it makes #!/usr/local/bin/suidperl
 	if ($string =~ /^\#\!\/usr\/local\/bin\/suidperl/) {
-		return 0;
+		return 1;
+	}
+
+	#check if it should possibly do the env check
+	if ( $self->{env} ){
+		if ( $string =~ /^\#!\/usr\/bin\/env.*perl/ ) {
+			return 1;
+		}
 	}
 
 	#not a perl script
-	return 1;
+	return undef;
 }
 
-=head1 RETURN
+=head1 ERROR CODES/FLAGS/HANDLING
 
 The easiest way to check is to verify the returned value is false.
 
-=head2 0
+Error handling is provided by L<Error::Helper>.
 
-It is a Perl script.
-
-=head2 1
-
-It is not a Perl script.
-
-=head2 2
+=head2 2, noString
 
 The string is not defined.
 
-=head2 3
+=head2 3, doesNotExist
 
 The file does not exist.
 
-=head2 4
+=head2 4, notReadable
 
 The file is not readable.
 
-=head2 5
+=head2 5, fileNotSpecified
 
 No file specified.
 
-=head2 6
+=head2 6, noFile
 
 The file could not be opened.
 
-=head2 7
+=head2 7, notAfile
 
 The specified file is not a file.
 
 =head1 AUTHOR
 
-Zane C. Bowers, C<< <vvelox at vvelox.net> >>
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
 
 =head1 BUGS
 
@@ -226,7 +305,7 @@ L<http://search.cpan.org/dist/Script-isAperlScript/>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2010 Zane C. Bowers, all rights reserved.
+Copyright 2012 Zane C. Bowers-Hadley, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
